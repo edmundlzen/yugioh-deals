83b8cd97c46c84c25e0fbe85653ed73acd76aebc
Edmund <edmundlzen@gmail.com>
2022-05-16 16:15:40 +0800

    Added logo and header to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Head from 'next/head'
+import {AppShell, Header} from "@mantine/core";
+import Image from "next/image";
 
 type Props = {
 	children: React.ReactNode
@@ -16,7 +18,20 @@ const Layout: React.FunctionComponent<Props> = ({
 			<meta charSet="utf-8"/>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
 		</Head>
-		{children}
+		<AppShell
+			padding="md"
+			header={<Header height={80} className={'flex items-center px-3'}>
+				<Image src="/static/images/logo.png" height={70} width={70}/>
+			</Header>
+			}
+			styles={(theme) => ({
+				main: {backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0]},
+			})}
+		>
+			<div className={'min-h-screen'}>
+				{children}
+			</div>
+		</AppShell>
 	</div>
 )
 
