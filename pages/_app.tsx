a85f42f6afe325ee9c315dc843138168f5d244f5
Edmund <edmundlzen@gmail.com>
2022-05-16 15:48:28 +0800

    Added Layout component and custom layout feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
+import Layout from "../components/Layout";
 
 function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />
+	// Check if the page has a custom layout.
+	if (Component.hasOwnProperty('getLayout')) {
+		const getLayout = (Component as any).getLayout;
+		return getLayout(<Component {...pageProps} />);
+	} else {
+		return (
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		)
+	}
 }
 
 export default App
